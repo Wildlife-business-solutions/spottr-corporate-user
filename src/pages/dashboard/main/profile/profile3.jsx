dc6b599d5fa887bbbae654de4915968cf137eb32
adeogojoshua <adeogojoshua10@gmail.com>
2024-01-15 14:27:16 +0100

    update

--- src/pages/dashboard/main/profile/profile3.jsx
+++ src/pages/dashboard/main/profile/profile3.jsx
@@ -13,6 +13,8 @@ import { Button } from "GlobalStyles/spotrrStyles/style";
 import Catalog from "assets/svg/catalog.svg";
 import Google from "assets/svg/google.svg";
 import backArrow from "assets/svg/backArrow.svg";
+import greenArrowUp from "assets/greenArrowUp.png"
+import redArrowDown from "assets/redArrowDown.png"
 import BarCode from "assets/svg/barcode.svg";
 import TagImage from "assets/tag.png";
 import BarChart from "assets/mem/barchart.png";
@@ -157,27 +159,18 @@ const Profile3 = () => {
             </p>
           </Div>
         </SpottrLink>
-        {search !== "?profile" && (
-          <Button
-            fw="bold"
-            fs="18px"
-            color="#275b89"
-            bc="#00000000"
-            onClick={() => handleFinish()}>
-            Finish
-          </Button>
-        )}
+        
       </Div>
 
       <Div gap="20px" display="flex" width="100%" justify="space-between">
 
+      <Div backgroundColor="#fff" padding="0 0 25px 0" width="50%">
       <img
               // src={userInfo?.avatar}
               src={search === "?profile" ? profile?.image : userInfo?.avatar}
               style={profileImage}
               alt="spottr user dp"
             />
-      <Div backgroundColor="#fff" padding="0 0 25px 0" width="50%">
           <Div padding="25px 35px">
             <Div display="flex" justify="space-between" alignI="flex-start">
               <Div>
@@ -231,8 +224,8 @@ const Profile3 = () => {
                 color="#275b89"
                 bc="#ecf7ff"
                 
-                fs="15px"
-                style={{ cursor: "pointer", padding:"10px 30px" }}>
+                fs="12px"
+                style={{ cursor: "pointer", padding:"5px 20px" }}>
                 Call
               </Button>
               <Button
@@ -240,8 +233,8 @@ const Profile3 = () => {
                 color="#275b89"
                 bc="#ecf7ff"
                 
-                fs="15px"
-                style={{ cursor: "pointer", padding:"10px 30px" }}>
+                fs="12px"
+                style={{ cursor: "pointer", padding:"5px 20px" }}>
                 Message
               </Button>
               <Button
@@ -249,8 +242,8 @@ const Profile3 = () => {
                 color="#275b89"
                 bc="#ecf7ff"
                 
-                fs="15px"
-                style={{ cursor: "pointer", padding:"10px 20px" }}>
+                fs="12px"
+                style={{ cursor: "pointer", padding:"5px 20px" }}>
                 My Barcode
               </Button>
             </Div>
@@ -300,10 +293,10 @@ const Profile3 = () => {
           <Div padding="25px 35px">
             <Div display="flex" justify="space-between" alignI="flex-start">
               <Div>
-                <Div fs="25px" color="#274B89" >
+                <Div fs="20px" color="#274B89" >
                 Performance
                 </Div>
-                <Div fs="13px" color="#929AA7">
+                <Div fs="11px" color="#929AA7">
                 Abdul sold 450 products in the month of March, compare to February
                 </Div>
                   <br/>
@@ -315,7 +308,7 @@ const Profile3 = () => {
                     <Div fs="13px" color="#929AA7" >Jan 6-7</Div>
                     <Div fs="13px" color="#929AA7" >Jan 9-11</Div>
                 </Div>
-                <br/>
+                
                 <Div display="flex" justify="space-between">
                     <Div fs="11px" color="#929AA7" >This Month</Div>
                     <Div fs="11px" color="#929AA7" >Last Month</Div>
@@ -324,28 +317,43 @@ const Profile3 = () => {
               </Div>
               
             </Div>
-            <Div display="flex" margin="2   0px 0" justify="space-between" alignI="flex-start">
+            <Div display="flex" margin="20px 0" justify="space-between" alignI="flex-start">
               <Div>
                 <Div fs="25px" color="#274B89" >
                 Performance metrics
                 </Div>
                 <Div fs="13px" color="#929AA7">
-                Stats compare to previous month                </Div>
+                Stats compare to previous month                
+                </Div>
                   <br/>
-                <img src={BarChart} alt="icon" />
+                
+                <Div display="flex" margin="" justify="space-between" width="100%">
+                    <Div alignI="">
+                        <Div fs="13px" fw="bold" color="#929AA7">Sales</Div>
+                        <Div fs="35px" fw="bold" color="#274B89" >495</Div>
+                        <Div fs="10px" color="#929AA7"><img src={greenArrowUp} width={15} alt="icon" /> 14%</Div>
+                    </Div>
+                    <Div alignI="">
+                        <Div fs="13px" fw="bold" color="#929AA7">Products</Div>
+                        <Div fs="35px" fw="bold" color="#274B89" >38</Div>
+                        <Div fs="10px" color="#929AA7"><img src={greenArrowUp} width={15} alt="icon" /> 78%</Div>
+                    </Div>
+                </Div>
 
-                <Div display="flex" justify="space-between">
-                    <Div fs="13px" color="#929AA7" >Jan 1-2</Div>
-                    <Div fs="13px" color="#929AA7" >Jan 2-5</Div>
-                    <Div fs="13px" color="#929AA7" >Jan 6-7</Div>
-                    <Div fs="13px" color="#929AA7" >Jan 9-11</Div>
+                <Div display="flex" margin="30px 0" justify="space-between" width="100%">
+                    <Div alignI="">
+                        <Div fs="13px" fw="bold" color="#929AA7">Lists</Div>
+                        <Div fs="35px" fw="bold" color="#274B89" >2</Div>
+                        <Div fs="10px" color="#929AA7"><img src={redArrowDown} width={15} alt="icon" /> 87%</Div>
+                    </Div>
+                    <Div alignI="">
+                        <Div fs="13px" fw="bold" color="#929AA7">Rating</Div>
+                        <Div fs="35px" fw="bold" color="#274B89" >4.5</Div>
+                        <Div fs="10px" color="#929AA7"><img src={greenArrowUp} width={15} alt="icon" /> 78%</Div>
+                    </Div>
                 </Div>
-                <br/>
-                <Div display="flex" justify="space-between">
-                    <Div fs="11px" color="#929AA7" >This Month</Div>
-                    <Div fs="11px" color="#929AA7" >Last Month</Div>
-                    
-                </Div>
+
+                
               </Div>
               
             </Div>
@@ -355,118 +363,71 @@ const Profile3 = () => {
         </Div>
       <Div backgroundColor="#fff" padding="0 0 25px 0" width="50%">
           <Div padding="25px 35px">
-            <Div display="flex" justify="space-between" alignI="flex-start">
-              <Div>
-                <Div fs="25px" color="#274B89" fw="bold">
-                Abdul, Mashir
-                </Div>
-                <Div fs="13px" color="#929AA7">
-                  {search === "?profile"
-                    ? profile.username
-                    : "@" + userInfo?.username}
-                </Div>
-              </Div>
-              <Div display="flex" flexDirection="column">
-                <Tag color="#274B89" bgcolor="rgba(227, 232, 240, 0.51)">
-                  <Img src={TagImage} width={16} height={16} />
-                  5,786 items sold
-                </Tag>
-                <Tag
-                  color="#E09B09"
-                  bgcolor="rgba(255, 178, 17, 0.15)"
-                  mg="10px 0 0 0">
-                  4,567 people rated
-                </Tag>
-              </Div>
+            <Div fs="20px" color="#274B89" >
+            Sales Highlights
             </Div>
-            <Div margin="35px 0 0 0" backgroundColor="#F8F8F8">
-              <Div
-                color="#000"
-                display="flex"
-                justify="space-between"
-                padding="10px 8px 8px 8px">
-                <h5 style={{ color: "rgba(0, 0, 0, 0.7)", fontWeight: 800 }}>
-                  Brief Info:
-                </h5>
-                <p
-                  style={{
-                    fontSize: "10px",
-                    fontWeight: 800,
-                    color: "#274889",
-                  }}>
-                  {userInfo.bio ? userInfo.bio.length : 0}/200
-                </p>
-              </Div>
-              I’ve worked directly, indirectly with these brands Either part time, full time, campaign, accelerator, branding or product design)
-            </Div>
-            
-            <Div margin="15px 0">
-            <Div margin="35px 0px 35px 0px" display="flex" justify="space-between" alignI="center">
-              <Button
-                fw="bold"
-                color="#275b89"
-                bc="#ecf7ff"
-                
-                fs="15px"
-                style={{ cursor: "pointer", padding:"10px 30px" }}>
-                Call
-              </Button>
-              <Button
-                fw="bold"
-                color="#275b89"
-                bc="#ecf7ff"
-                
-                fs="15px"
-                style={{ cursor: "pointer", padding:"10px 30px" }}>
-                Message
-              </Button>
-              <Button
-                fw="bold"
-                color="#275b89"
-                bc="#ecf7ff"
-                
-                fs="15px"
-                style={{ cursor: "pointer", padding:"10px 20px" }}>
-                My Barcode
-              </Button>
-            </Div>
-            <Divider />
-              
-            <Div margin="15px 0px 15px 0px" justify="space-between" alignI="center">
-                <Div fs="12px" color="#929AA7" margin="0 0 10px 0">
-                  Email Address
-                </Div>
-                <Div fs="20px" color="#274B89" fw="bold">
-                    adedamolamoses@gmail.com
-                </Div>
-            </Div>
-            <Divider />
-              
-            <Div margin="15px 0px 15px 0px" justify="space-between" alignI="center">
-                <Div fs="12px" color="#929AA7" margin="0 0 20px 0">
-                  Country
-                </Div>
-                <Div fs="20px" alignI="left" color="#274B89" fw="bold">
-
-                    <Div fs="20px" color="#274B89" fw="bold">
-                    <Img src={NigImage}  />
-                        &nbsp;&nbsp;
-                        Lagos
+                <Div display="flex" margin="20px 0" justify="space-between">
+                    <Div>
+                        <Div fs="15px" fw="bold" color="#3B3B3BB2">5769</Div>
+                        <Div fs="10px" fw="bold" color="#929AA7">Coca-cola</Div>
+                    </Div>
+                    <Div>
+                        <Div fs="10px" fw="bold" color="#FF0000B2">-63.8%</Div>
                     </Div>
                 </Div>
+                <Div display="flex" margin="20px 0" justify="space-between">
+                    <Div>
+                        <Div fs="15px" fw="bold" color="#3B3B3BB2">30</Div>
+                        <Div fs="10px" fw="bold" color="#929AA7">Fanta</Div>
+                    </Div>
+                    <Div>
+                        <Div fs="10px" fw="bold" color="#39B54A">+8%</Div>
+                    </Div>
+                </Div>
+                <Div display="flex" margin="20px 0" justify="space-between">
+                    <Div>
+                        <Div fs="15px" fw="bold" color="#3B3B3BB2">5769</Div>
+                        <Div fs="10px" fw="bold" color="#929AA7">Coca-cola</Div>
+                    </Div>
+                    <Div>
+                        <Div fs="10px" fw="bold" color="#FF0000B2">-63.8%</Div>
+                    </Div>
+                </Div>
+                <Div display="flex" margin="20px 0" justify="space-between">
+                    <Div>
+                        <Div fs="15px" fw="bold" color="#3B3B3BB2">30</Div>
+                        <Div fs="10px" fw="bold" color="#929AA7">Fanta</Div>
+                    </Div>
+                    <Div>
+                        <Div fs="10px" fw="bold" color="#39B54A">+8%</Div>
+                    </Div>
+                </Div>
+            
+            
+            <Div margin="15px 0">
+            
+            <Divider />
+            <Div margin="15px 0px 15px 0px" justify="space-between" alignI="center">
+                <Div fs="20px" color="#fff" fw="bold">
+                    adedamolamoses@gmail.com
+                </Div>
+                <Div fs="20px" color="#274B89" fw="bold">
+                Share Adbul’s profile
+                </Div>
+                
+            </Div>
+            <Divider />
+            <Div margin="15px 0px 15px 0px" justify="space-between" alignI="center">
+                <Div fs="20px" color="#fff" fw="bold">
+                    adedamolamoses@gmail.com
+                </Div>
+                <Div fs="16px" color="#FF4B3E" fw="bold">
+                Blacklist this user
+                </Div>
+                
             </Div>
             <Divider />
               
-            <Div margin="15px 0px 15px 0px" justify="space-between" alignI="center">
-                <Div fs="12px" color="#929AA7" margin="0 0 20px 0">
-                  Login Type
-                </Div>
-                <Div fs="20px" display="flex" justify="space-between" alignI="left" color="#274B89" fw="bold">
-                    <Img src={Google}  />
-
-                    
-                </Div>
-            </Div>
               
             </Div>
          
